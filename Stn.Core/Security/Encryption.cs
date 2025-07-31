607d6b5004bebc36d9ef90b27bfed8d69e144d17
Laurens Ruijtenberg <laurens.ruijtenberg@outlook.com>
2025-07-31 15:16:58 +0200

    small bugfix

--- Stn.Core/Security/Encryption.cs
+++ Stn.Core/Security/Encryption.cs
@@ -77,32 +77,39 @@ namespace Stn.Core.Security
 
         public static string DecryptString(string input)
         {
-            var key = GetAes256KeyFromString(Key); // 32 bytes
-            var fullCipher = Convert.FromBase64String(input);
+            try {
+                var key = GetAes256KeyFromString(Key); // 32 bytes
+                var fullCipher = Convert.FromBase64String(input);
 
-            // Extract IV (first 16 bytes)
-            var iv = new byte[16];
-            Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+                // Extract IV (first 16 bytes)
+                var iv = new byte[16];
+                Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
 
-            // Extract ciphertext
-            var cipherText = new byte[fullCipher.Length - iv.Length];
-            Buffer.BlockCopy(fullCipher, iv.Length, cipherText, 0, cipherText.Length);
+                // Extract ciphertext
+                var cipherText = new byte[fullCipher.Length - iv.Length];
+                Buffer.BlockCopy(fullCipher, iv.Length, cipherText, 0, cipherText.Length);
 
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = key;
-                aesAlg.IV = iv;
-                aesAlg.Mode = CipherMode.CBC;
-                aesAlg.Padding = PaddingMode.PKCS7;
-
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                using (var msDecrypt = new MemoryStream(cipherText))
-                using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                using (var srDecrypt = new StreamReader(csDecrypt))
+                using (Aes aesAlg = Aes.Create())
                 {
-                    return srDecrypt.ReadToEnd();
+                    aesAlg.Key = key;
+                    aesAlg.IV = iv;
+                    aesAlg.Mode = CipherMode.CBC;
+                    aesAlg.Padding = PaddingMode.PKCS7;
+
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                    using (var msDecrypt = new MemoryStream(cipherText))
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    using (var srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
                 }
+            } catch
+            {
+                // If decryption fails, return the original string
+                // we need to improve this.
+                return input;
             }
         }   
     }
